369669b6bb28593f449f81df99c035e8238b297e
Trishant Naidu <trishant.naidu.vc@startv.com>
2020-01-16 19:33:11 +0530

    Updates

---
+++ precache-manifest.ec524e321047d8ed65e5d905a1978fe5.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "346c1e43dfc424ddc664b6b2fc0743fe",
+    "url": "/analytics-dashboard/index.html"
+  },
+  {
+    "revision": "07f2b1155d5113ba88c8",
+    "url": "/analytics-dashboard/static/css/main.eb5df336.chunk.css"
+  },
+  {
+    "revision": "fe3099a7d2c0728af654",
+    "url": "/analytics-dashboard/static/js/2.af14a4d2.chunk.js"
+  },
+  {
+    "revision": "07f2b1155d5113ba88c8",
+    "url": "/analytics-dashboard/static/js/main.643a40c0.chunk.js"
+  },
+  {
+    "revision": "99ed404297caef2e3f78",
+    "url": "/analytics-dashboard/static/js/runtime-main.9d622555.js"
+  },
+  {
+    "revision": "6ffdbdd5efb51cd3e6dc5716f1cc33db",
+    "url": "/analytics-dashboard/static/media/Zapr.6ffdbdd5.csv"
+  },
+  {
+    "revision": "bd07ee7bbf971ac37295f9c4469c17ea",
+    "url": "/analytics-dashboard/static/media/logo.bd07ee7b.svg"
+  },
+  {
+    "revision": "ae4cae7785eb45c1e282ecba1d3f1948",
+    "url": "/analytics-dashboard/static/media/logoDark.ae4cae77.svg"
+  },
+  {
+    "revision": "376b7144350746e43a3d4daf2148045b",
+    "url": "/analytics-dashboard/static/media/region-mappings.376b7144.csv"
+  },
+  {
+    "revision": "064af8a4a82a895f276deb298eaadc52",
+    "url": "/analytics-dashboard/static/media/section00.064af8a4.svg"
+  },
+  {
+    "revision": "c7b4398f41a2813ad1a81031945096c3",
+    "url": "/analytics-dashboard/static/media/slack.c7b4398f.svg"
+  },
+  {
+    "revision": "54a84bb9e90027afd3b2d0ac6a71ad4a",
+    "url": "/analytics-dashboard/static/media/state-mappings.54a84bb9.csv"
+  }
+]);
